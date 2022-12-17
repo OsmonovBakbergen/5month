dfd22265d4010c14a1bb5a732c26e5bf6278fa56
OsmonovBakbergen <osmonovbaku@gmail.com>
2022-12-17 16:57:44 +0600

    hw1

---
+++ movie_app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from movie_app.seralizers import *
+from movie_app.models import Movie, Director, Review
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view(['GET'])
+def movies_view(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def movie_detail_view(request, **kwargs):
+    movie = Movie.objects.get(id=kwargs['id'])
+    serializer = MovieSerializer(movie)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def directors_view(request):
+    directors = Director.objects.all()
+    serializer = DirectorSerializer(directors, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def director_detail_view(request, **kwargs):
+    director = Director.objects.get(id=kwargs['id'])
+    serializer = DirectorSerializer(director)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def reviews_view(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_detail_view(request, **kwargs):
+    review = Review.objects.get(id=kwargs['id'])
+    serializer = ReviewSerializer(review)
+    return Response(data=serializer.data)
